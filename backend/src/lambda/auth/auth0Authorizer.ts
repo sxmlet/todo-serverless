bd6f28f0090f29033bc21859cc1ed177c1955043
Peter Pajor <saxumvermes@protonmail.com>
2023-04-22 23:51:23 +0200

    update readme

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,6 @@ import { JwtPayload } from '../../auth/JwtPayload'
 import { JWK } from '../../auth/JWK.js'
 
 const logger = createLogger('auth')
-
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl = 'https://dev-d2mjz3ehsqoecv3g.us.auth0.com/.well-known/jwks.json';
 
 export const handler = async (
